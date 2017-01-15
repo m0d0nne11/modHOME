f341cd20e0cc789d2881f034ea4051dc73dd13c7
m0d0nne11 <mod.github@b0rken.com>
2017-01-15 11:21:16 -0500

    Not sure why these were missing but re-adding them here.

---
+++ local/script/MHfilesLinkedToSameInode.sh
@@ -0,0 +1,45 @@
+#
+# Found in my MH mail hierarchy a number of unexplained
+# instances where various files were multiply linked to the
+# same inode so I wrote this script to report such instances
+# and, in the case when one file is a "live" file (ie. has no
+# comma in the name) to recommend deletion of the others.
+# NOTE: these multi-linked files seem to be part of MH's
+# approach to deleting/sorting/relocating message files and
+# are not a "problem"
+#
+
+MH_HIERARCHY_ROOT=/home/mod/.mail
+
+function listOfMHfilesWithBetween2and7dirLinks() {
+    local i;
+    local l;
+    local f;
+    for i in $(for l in $(seq 2 7) ; do find . -type f -links $l ; done \
+             | while read f ; do ls -id1 $f ; done | word1 | sort -nu);
+    do
+        echo $(find . -inum $i | sed -e s/..// );
+    done
+}
+
+function MHfilesLinkedToSameInode() {
+    local c;    # Comma files
+    local cc;   # Comma files(count)
+    local l;
+    local x;    # Normal files
+    local xc;   # Normal files(count)
+    while read l; do
+        c=$(echo "$l" | fmt -1 | fgrep    -e ',');
+        x=$(echo "$l" | fmt -1 | fgrep -v -e ',');
+        cc=$(echo "$c" | wc -w);
+        xc=$(echo "$x" | wc -w);
+        if [ $xc -eq 1 -a $cc -ge 1 ]; then
+            echo KEEP $x TOSS $c;
+        else
+            echo INVESTIGATE X:$xc C:$cc $x $c;
+        fi;
+    done
+}
+
+cd $MH_HIERARCHY_ROOT && \
+  listOfMHfilesWithBetween2and7dirLinks | MHfilesLinkedToSameInode
---
+++ local/script/symlinkChain.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# set -x
+
+startDir=$PWD
+#append="\n\t"
+append=" => "
+
+for f in $* ; do
+	chain="${f}"
+
+	if [ -L "${f}" ] ; then
+	        while [ -L "${f}" ] ; do
+	                d1="$(dirname "${f}")"
+	                cd "${d1}"
+	                l="$(readlink "${f}")"
+	                d2="$(cd $(dirname "${l}") ; echo $PWD)"
+	                f="${d2}/$(basename ${l})"
+	                chain="${chain}${append}${f}"
+	                cd "${d2}"
+	        done
+	fi
+
+	echo -e "${chain}"
+done
