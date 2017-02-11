ac4abf6044af735c99ec1bcf93db997eccf9771a
Michael O'Donnell <Michael.ODonnell@ll.mit.edu>
2017-02-10 20:11:42 -0500

    Add handling of individual files as well as directories.  Tidy comments.

--- local/script/shuttle.sh
+++ local/script/shuttle.sh
@@ -1,24 +1,58 @@
 #!/bin/bash
 
 ###############################################################################
-# Use rsync to shuttle hierarchies back and forth between systems.
-#
-# Specify pathnames for root nodes of hierarchies below, between
-# the "yourDirectoryListHere" markers, one per line.  Specified
-# hierarchies must exist relative to home dir on both hosts.
+# Script using rsync to shuttle hierarchies back and forth between systems.
 #
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# BEGIN CONFIGURABLES:
+
+otherHost=otherHostNotYetDefined
+
+unset otherUser     # Defaults to current user when undefined/unset
+#otherUser=MI25714@ # NOTE: if set, otherUser must have trailing @
+#otherUser=mi25714@ # NOTE: if set, otherUser must have trailing @
+
+# Modify utterDirList() here to specify pathnames for root directories
+# (not individual files) of any hierarchies to be recursively rsync'd.
+# Those pathnames:
+#  - must appear 1 per line between the "DIR_LIST_BOUNDARY" lines.
+#  - are assumed to be pathnames relative to the home directory;
+#    absolute paths may also work but have not been exercised...
+
+function utterDirList() {
+    cat <<"DIR_LIST_BOUNDARY"
+your/Directory/here
+DIR_LIST_BOUNDARY
+}
+
+# Modify utterFileList() here to specify pathnames for individual
+# files to be rsync'd.  Those pathnames:
+#  - must appear 1 per line between the "FILE_LIST_BOUNDARY" lines.
+#  - are assumed to be pathnames relative to the home directory;
+#    absolute paths may also work but have not been exercised...
+
+function utterFileList() {
+    cat <<"FILE_LIST_BOUNDARY"
+your/individual/File/here
+FILE_LIST_BOUNDARY
+}
+
+# END CONFIGURABLES
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #
 function usage() {
     echo ""
     echo USAGE:
-    echo "   ${myName}" -[io] [-d]
-    echo Syncs specified hierarchies inbound/outbound via
-    echo  rsync with other host "(currently '${otherHost}')"
-    echo  ""
-    echo  WARNING: the -d option accomplishes complete sync by
-    echo  permiting ruthless deletion of destination files.
+    echo "   ${scriptName}" -[io] [-d] [-F]
+    echo Syncs the specified hierarchies inbound/outbound via
+    echo rsync with other host "(currently '${otherHost}')"
+    echo "One of -i or -o must be present"
+    echo "-d optionally specifies '--delete'"
+    echo "-F optionally specifies '--dry-run'"
+    echo ""
+    echo WARNING: the -d option accomplishes complete sync by
+    echo permitting ruthless deletion of destination files...
 }
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
@@ -33,26 +67,22 @@ function FAILED()  {
         msg="${*}"
     fi
 
-    echo "${myName} FAILED: ${msg}" >&2
+    echo "${scriptName} FAILED: ${msg}" >&2
     usage
     exit 1
 }
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-   myName="${0}"
-    myPID=$$
-otherHost=otherHostNotYetDefined
+scriptName="${0}"
+ scriptPID=$$
 
 unset direction # No default
 
-unset otherUser     # Defaults to current user
-#otherUser=MI25714@ # NOTE: if set, otherUser must have trailing @
-#otherUser=mi25714@ # NOTE: if set, otherUser must have trailing @
-
-options=""
+      options=""
 currentOptArg=""
-while getopts "iod" currentOptArg ; do
+
+while getopts "iodF" currentOptArg ; do
     case "$currentOptArg" in
     '?')                                                # End of supplied args.
         FAILED unexpected option
@@ -65,6 +95,9 @@ while getopts "iod" currentOptArg ; do
         ;;
     d)
         options="${options} --delete" # Permit ruthless deletion of dest files.
+        ;;
+    F)
+        options="${options} --dry-run" # Fake it.
         ;;
     *)
         failed "Unexpected commandline parameter: $currentOptArg"
@@ -82,26 +115,43 @@ cd ~               || FAILED "Cannot cd ~ ?"
 
   #  #  #  #  #  #  #  #
 
-while read dir ; do
-    [ -d "${dir}"/. ] || FAILED "${dir}" "...not a Directory?"
+# The main loops...
+
+utterDirList | while read dir ; do
     if ! [ "${dir}" ] ; then
-        echo '  #### SKIPPING null entry - blank line between yourDirectoryListHere tags?'
+        echo '  #### SKIPPING null entry - blank line between DIR_LIST_BOUNDARY tags?'
         continue
     fi
-    echo "  #### BEGIN $direction ${otherUser}${otherHost}":"'${dir}'"/.
+    if ! [ -d "${dir}"/. ] ; then
+        echo ERROR - "${dir} not a Directory?"
+        continue
+    fi
+    echo "  #### BEGIN $direction ${options} ${otherUser}${otherHost}":"'${dir}'"/.
 
     if [ "$direction" = "outbound" ] ; then
         rsync -vax ${options} "${dir}"/. ${otherUser}"${otherHost}":"'${dir}'"/.
     else
         rsync -vax ${options}            ${otherUser}"${otherHost}":"'${dir}'"/. "${dir}"/.
     fi
-    echo "  ##### DONE $direction ${otherUser}${otherHost}":"'${dir}'"/.
+    echo "  ##### DONE $direction ${options} ${otherUser}${otherHost}":"'${dir}'"/.
+done
 
-done <<"yourDirectoryListHere"
-Desktop/exampleShuttleDirectory
-yourDirectoryListHere
+utterFileList | while read f ; do
+    if ! [ "${f}" ] ; then
+        echo '  #### SKIPPING null entry - blank line between FILE_LIST_BOUNDARY tags?'
+        continue
+    fi
+    if ! [ -f "${f}" ] ; then
+        echo ERROR - "${f} not a File?"
+        continue
+    fi
+    echo "  #### BEGIN $direction ${options} ${otherUser}${otherHost}":"'${f}'"
 
-# Specify (in the list above) root nodes of hierarchies to transfer,
-# one per line, between the "yourDirectoryListHere" lines.  Assumed
-# to be relative to home directory; absolute paths may also work...
-#
+    if [ "$direction" = "outbound" ] ; then
+        rsync -vax ${options} "${f}" ${otherUser}"${otherHost}":"'${f}'"
+    else
+        rsync -vax ${options}        ${otherUser}"${otherHost}":"'${f}'" "${f}"
+    fi
+    echo "  ##### DONE $direction ${options} ${otherUser}${otherHost}":"'${f}'"
+done
+
