f3bebf17a4fe118313374c18409b451074813cc3
Michael O'Donnell <MI25714@vmobuntu1404.llan.ll.mit.edu>
2017-03-28 11:19:29 -0400

    Add (incomplete) logic to use either dashes or underscores as the dummy character.

--- local/script/crunchXLS.sh
+++ local/script/crunchXLS.sh
@@ -2,12 +2,15 @@
 
 # ASSUME: stdin is the cut'n'pasted data from a MacOSX Excel spreadsheet.
 #
-# Prepare the data such that blank cells have a single dash in them
-# so that "lineup" will do the right thing, and also replace all Space
-# chars with underscores to keep cell contents containing spaces from
-# confusing "lineup" into thinking they're multiple cells.
+# Prepare the data such that blank cells have a single (dash or
+# underscore) in them so that "lineup" will do the right thing,
+# and also replace all Space chars with (dashes or underscores)
+# to keep cell contents containing spaces from confusing "lineup"
+# into thinking they're multiple cells.
 #
 
+# This is the DASH version:
+#
 sed -e 's/^	/-	/g' \
     -e 's/	(blank)	/	-	/g' \
     -e 's/	(blank)	/	-	/g' \
@@ -15,4 +18,18 @@ sed -e 's/^	/-	/g' \
     -e 's/		/	-	/g' \
     -e 's/		/	-	/g' \
     -e 's/ /-/g' | lineup
+
+exit
+
+# This is the UNDERSCORE version:
+#
+sed -e 's/^	/_	/g' \
+    -e 's/	(blank)	/	_	/g' \
+    -e 's/	(blank)	/	_	/g' \
+    -e 's/	(blank)$/	_/g' \
+    -e 's/		/	_	/g' \
+    -e 's/		/	_	/g' \
+    -e 's/ /_/g' | lineup
+
+exit
 
