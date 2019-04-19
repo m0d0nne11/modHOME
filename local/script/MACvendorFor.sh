8333e04982e2d1518b90ec769e9fd44edba7dcfe
root <root@opnetsim7.llan.ll.mit.edu>
2019-04-19 08:27:34 -0400

    Capture suggested logic from vendor-ID site. Evolve some tweaks.

--- local/script/MACvendorFor.sh
+++ local/script/MACvendorFor.sh
@@ -111,3 +111,21 @@ fi ;
 
 exit ;
 
+
+
+From the macvendors.com WWW site:
+
+<?php
+  $mac_address = "FC:FB:FB:01:FA:21";
+  $url = "https://api.macvendors.com/" . urlencode($mac_address);
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  $response = curl_exec($ch);
+  if($response) {
+    echo "Vendor: $response";
+  } else {
+    echo "Not Found";
+  }
+?>
+
